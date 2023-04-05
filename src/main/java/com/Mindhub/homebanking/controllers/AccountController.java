68ebd3af4d161eb45608e1e98fcbff0596bd7a1e
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-05 12:57:14 -0300

    Added a RequestMapping to /accounts/{id}

--- src/main/java/com/Mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/Mindhub/homebanking/controllers/AccountController.java
@@ -3,10 +3,12 @@ package com.Mindhub.homebanking.controllers;
 import com.Mindhub.homebanking.dtos.AccountDTO;
 import com.Mindhub.homebanking.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
@@ -17,10 +19,14 @@ public class AccountController {
     private AccountRepository accountRepository;
 
     @RequestMapping("/accounts")
-    public List<AccountDTO> getAccount(){
+    public List<AccountDTO> getAccounts(){
         return accountRepository.findAll()
                 .stream()
                 .map(account -> new AccountDTO(account))
                 .collect(toList());
+    }
+    @RequestMapping("/accounts/{id}")
+    public Optional<AccountDTO> getAccount(@PathVariable Long id){
+        return accountRepository.findById(id).map((account -> new AccountDTO(account)));
     }
 }
