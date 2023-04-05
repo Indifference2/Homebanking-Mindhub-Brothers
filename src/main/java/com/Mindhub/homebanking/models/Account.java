4cd39aab7595383a43c615c73a472a657a54b878
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-05 10:43:04 -0300

    Make relations ManyToOne in Class Transactions and OneToMany in Class Account

--- src/main/java/com/Mindhub/homebanking/models/Account.java
+++ src/main/java/com/Mindhub/homebanking/models/Account.java
@@ -4,6 +4,8 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Account {
@@ -11,54 +13,34 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "client_id")
-    private Client client;
     private String number;
     private LocalDateTime creationDate;
     private double balance;
-
-    public Account(){
-    }
-
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private Client client;
+    @OneToMany(mappedBy = "account", fetch = FetchType.EAGER)
+    Set<Transaction> transactions = new HashSet<>();
+    public Account(){}
     public Account(String number, double balance, LocalDateTime creationDate){
         this.number = number;
         this.balance = balance;
         this.creationDate = creationDate;
     }
-
-
-    public String getNumber() {
-        return number;
+    public void addTransaction(Transaction transaction){
+        transaction.setAccount(this);
+        transactions.add(transaction);
     }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public Client getClient() {
-        return client;
-    }
-
-    public void setClient(Client client) {
-        this.client = client;
-    }
+    public Set<Transaction> getTransactions(){return transactions;}
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+    public String getNumber() {return number;}
+    public void setNumber(String number) {this.number = number;}
+    public LocalDateTime getCreationDate() {return creationDate;}
+    public void setCreationDate(LocalDateTime creationDate) {this.creationDate = creationDate;}
+    public double getBalance() {return balance;}
+    public void setBalance(double balance) {this.balance = balance;}
+    public Client getClient() {return client;}
+    public void setClient(Client client) {this.client = client;}
 }
 
