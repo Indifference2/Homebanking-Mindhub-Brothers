5200e7e310ce19fc2cf01137b14644b04a0e2c1d
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-05 00:59:49 -0300

    Change syntax getters and setters

--- src/main/java/com/Mindhub/homebanking/dtos/AccountDTO.java
+++ src/main/java/com/Mindhub/homebanking/dtos/AccountDTO.java
@@ -8,34 +8,15 @@ public class AccountDTO {
     private String number;
     private LocalDateTime creationMode;
     private double balance;
-
     public AccountDTO(Account account){
         this.number = account.getNumber();
         this.creationMode = account.getCreationDate();
         this.balance = account.getBalance();
     }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public LocalDateTime getCreationMode() {
-        return creationMode;
-    }
-
-    public void setCreationMode(LocalDateTime creationMode) {
-        this.creationMode = creationMode;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
+    public String getNumber() {return number;}
+    public void setNumber(String number) {this.number = number;}
+    public LocalDateTime getCreationMode() {return creationMode;}
+    public void setCreationMode(LocalDateTime creationMode) {this.creationMode = creationMode;}
+    public double getBalance() {return balance;}
+    public void setBalance(double balance) {this.balance = balance;}
 }
