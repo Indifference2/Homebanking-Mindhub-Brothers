70da4db9286cd389562c59794ea41c38828bf4a7
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-05 10:50:03 -0300

    Create some transactions

--- src/main/java/com/Mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/Mindhub/homebanking/HomebankingApplication.java
@@ -2,14 +2,19 @@ package com.Mindhub.homebanking;
 
 import com.Mindhub.homebanking.models.Account;
 import com.Mindhub.homebanking.models.Client;
+import com.Mindhub.homebanking.models.Transaction;
 import com.Mindhub.homebanking.repositories.ClientRepository;
 import com.Mindhub.homebanking.repositories.AccountRepository;
+import com.Mindhub.homebanking.repositories.TransactionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
+
+import static com.Mindhub.homebanking.models.TransactionType.CREDIT;
+import static com.Mindhub.homebanking.models.TransactionType.DEBIT;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -18,7 +23,7 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner initData(ClientRepository repository, AccountRepository accountRepository) {
+	public CommandLineRunner initData(ClientRepository repository, AccountRepository accountRepository, TransactionRepository transactionRepository) {
 		return (args) -> {
 			// save a couple of customers
 			Client cliente1 = new Client("Melba", "Morel", "melba@mindhub.com");
@@ -26,12 +31,29 @@ public class HomebankingApplication {
 
 			Account account1 = new Account("VIN001", 5000, LocalDateTime.now());
 			Account account2 = new Account("VIN002", 7500, LocalDateTime.now().plusDays(1));
-
 			cliente1.addAccount(account1);
 			cliente1.addAccount(account2);
+
+			Transaction transaction1 = new Transaction(CREDIT, 3250.50, "Ebay", LocalDateTime.now());
+			Transaction transaction2 = new Transaction(DEBIT, 550.5, "Amazon", LocalDateTime.now().plusDays(2));
+			Transaction transaction3 = new Transaction(CREDIT, 8000.10, "Google", LocalDateTime.now().plusDays(2));
+			Transaction transaction4 = new Transaction(DEBIT, 660.20, "Alibaba", LocalDateTime.now().plusDays(6));
 			accountRepository.save(account1);
 			accountRepository.save(account2);
 
+
+			account1.addTransaction(transaction1);
+			account1.addTransaction(transaction2);
+			account2.addTransaction(transaction3);
+			account2.addTransaction(transaction4);
+
+			transactionRepository.save(transaction1);
+			transactionRepository.save(transaction2);
+			transactionRepository.save(transaction3);
+			transactionRepository.save(transaction4);
+
+			accountRepository.save(account1);
+			accountRepository.save(account2);
 
 		};
 	}
