f9d4c57ce5b9fd010d9a13da9a463f826546fdff
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-05 10:44:54 -0300

    Added transaction property

--- src/main/java/com/Mindhub/homebanking/dtos/AccountDTO.java
+++ src/main/java/com/Mindhub/homebanking/dtos/AccountDTO.java
@@ -1,17 +1,21 @@
 package com.Mindhub.homebanking.dtos;
 
 import com.Mindhub.homebanking.models.Account;
+import com.Mindhub.homebanking.models.Transaction;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public class AccountDTO {
     private String number;
     private LocalDateTime creationMode;
     private double balance;
+    private Set<Transaction> transaction;
     public AccountDTO(Account account){
         this.number = account.getNumber();
         this.creationMode = account.getCreationDate();
         this.balance = account.getBalance();
+        this.transaction = account.getTransactions();
     }
     public String getNumber() {return number;}
     public void setNumber(String number) {this.number = number;}
@@ -19,4 +23,5 @@ public class AccountDTO {
     public void setCreationMode(LocalDateTime creationMode) {this.creationMode = creationMode;}
     public double getBalance() {return balance;}
     public void setBalance(double balance) {this.balance = balance;}
+    public Set<Transaction> getTransaction() {return transaction;}
 }
