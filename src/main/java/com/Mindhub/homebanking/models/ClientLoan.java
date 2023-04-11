933c0c2a45299872f14f28669ec6adbc761dc4ac
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-11 17:27:46 -0300

    added getters and setters, and relation Many to one Client, Loan

--- src/main/java/com/Mindhub/homebanking/models/ClientLoan.java
+++ src/main/java/com/Mindhub/homebanking/models/ClientLoan.java
@@ -2,10 +2,7 @@ package com.Mindhub.homebanking.models;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class ClientLoan {
@@ -14,6 +11,33 @@ public class ClientLoan {
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
     private double amount;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="client_id")
     private Client client;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="loan_id")
     private Loan loan;
+
+    public ClientLoan(){}
+    public ClientLoan(Client client, double amount, Loan loan){
+        this.client = client;
+        this.amount = amount;
+        this.loan = loan;
+    }
+
+    public long getId() {return id;}
+
+    public void setId(long id) {this.id = id;}
+
+    public double getAmount() {return amount;}
+
+    public void setAmount(double amount) {this.amount = amount;}
+
+    public Client getClient() {return client;}
+
+    public void setClient(Client client) {this.client = client;}
+
+    public Loan getLoan() {return loan;}
+
+    public void setLoan(Loan loan) {this.loan = loan;}
 }
