8bb95cd6f388939336da7e2ff38ce8e440b75b83
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-11 16:37:01 -0300

    Create a LoanRepository

---
+++ src/main/java/com/Mindhub/homebanking/models/Loan.java
@@ -0,0 +1,2 @@
+package com.Mindhub.homebanking.models;public class Loan {
+}
---
+++ src/main/java/com/Mindhub/homebanking/repositories/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.Mindhub.homebanking.repositories;
+
+import com.Mindhub.homebanking.models.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+}
