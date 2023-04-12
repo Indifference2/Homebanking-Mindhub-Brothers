b5a5bef37b2a09485041c3304d30d79692246434
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-12 14:15:50 -0300

    Added properties payments and nameLoan, also change the constructor parameters

--- src/main/java/com/Mindhub/homebanking/models/ClientLoan.java
+++ src/main/java/com/Mindhub/homebanking/models/ClientLoan.java
@@ -10,6 +10,8 @@ public class ClientLoan {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
+    private String nameLoan;
+    private int payments;
     private double amount;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="client_id")
@@ -19,14 +21,16 @@ public class ClientLoan {
     private Loan loan;
 
     public ClientLoan(){}
-    public ClientLoan(Client client, double amount, Loan loan){
-        this.client = client;
+    public ClientLoan(String nameLoan, double amount, int payments){
         this.amount = amount;
-        this.loan = loan;
+        this.nameLoan = nameLoan;
+        this.payments = payments;
     }
-
+    public int getPayments() {return payments;}
+    public void setPayments(int payments) {this.payments = payments;}
+    public String getNameLoan() {return nameLoan;}
+    public void setNameLoan(String nameLoan) {this.nameLoan = nameLoan;}
     public long getId() {return id;}
-    public void setId(long id) {this.id = id;}
     public double getAmount() {return amount;}
     public void setAmount(double amount) {this.amount = amount;}
     public Client getClient() {return client;}
