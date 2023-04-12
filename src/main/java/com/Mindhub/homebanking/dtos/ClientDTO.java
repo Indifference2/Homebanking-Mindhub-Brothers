73b443d3633cc88c69e809e922025eb5c51913d4
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-12 20:22:13 -0300

    Added property Set<AccountDTO>, ClientLoadDTO.

--- src/main/java/com/Mindhub/homebanking/dtos/ClientDTO.java
+++ src/main/java/com/Mindhub/homebanking/dtos/ClientDTO.java
@@ -2,30 +2,36 @@ package com.Mindhub.homebanking.dtos;
 
 import com.Mindhub.homebanking.models.Client;
 import java.util.Set;
-import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toSet;
+
 public class ClientDTO {
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
-    private Set<AccountDTO> account;
+    private Set<AccountDTO> accounts;
+    private Set<ClientLoanDTO> clientLoans;
 
     public ClientDTO(Client client){
         this.id = client.getId();
         this.firstName = client.getFirstName();
         this.lastName = client.getLastName();
         this.email = client.getEmail();
-        this.account = client.getAccounts().
+        this.accounts = client.getAccounts().
                 stream()
                 .map(account -> new AccountDTO(account))
-                .collect(Collectors.toSet());
+                .collect(toSet());
+        this.clientLoans = client.getClientLoans()
+                .stream()
+                .map(clientLoan -> new ClientLoanDTO(clientLoan))
+                .collect(toSet());
     }
 
     public Long getId() {
         return id;
     }
     public Set<AccountDTO> getAccount() {
-        return account;
+        return accounts;
     }
     public String getFirstName() {
         return firstName;
@@ -36,4 +42,6 @@ public class ClientDTO {
     public String getEmail() {
         return email;
     }
+
+    public Set<ClientLoanDTO> getClientLoans() {return clientLoans;}
 }
---
+++ src/main/java/com/Mindhub/homebanking/dtos/ClientLoanDTO.java
@@ -0,0 +1,2 @@
+package com.Mindhub.homebanking.dtos;public class ClientLoanDTO {
+}
