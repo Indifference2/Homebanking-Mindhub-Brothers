262e92254c56ef7aabc50dec89592ce2effb99ef
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-12 15:48:22 -0300

    delete method setter id

--- src/main/java/com/Mindhub/homebanking/models/Client.java
+++ src/main/java/com/Mindhub/homebanking/models/Client.java
@@ -48,9 +48,6 @@ public class Client {
     public long getId() {
         return id;
     }
-    public void setId(long id) {
-        this.id = id;
-    }
     public String getFirstName() {
         return firstName;
     }
