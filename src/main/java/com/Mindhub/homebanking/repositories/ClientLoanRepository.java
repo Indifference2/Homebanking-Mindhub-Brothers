4a7d3045918fdf9a630dd72b23c22794e6d1b571
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-12 16:25:10 -0300

    Created ClientLoanRepository

--- src/main/java/com/Mindhub/homebanking/repositories/ClientLoanRepository.java
+++ src/main/java/com/Mindhub/homebanking/repositories/ClientLoanRepository.java
@@ -1,2 +1,9 @@
-package com.Mindhub.homebanking.repositories;public interface ClientLoanRepository {
+package com.Mindhub.homebanking.repositories;
+
+import com.Mindhub.homebanking.models.ClientLoan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientLoanRepository extends JpaRepository<ClientLoan, Long> {
 }
