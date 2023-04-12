a876f8fa5ff378f7d67ea9ca5cf3e2c344753c4b
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-12 20:23:41 -0300

    Created ClientLoanDTO with his properties

--- src/main/java/com/Mindhub/homebanking/dtos/ClientLoanDTO.java
+++ src/main/java/com/Mindhub/homebanking/dtos/ClientLoanDTO.java
@@ -1,2 +1,22 @@
-package com.Mindhub.homebanking.dtos;public class ClientLoanDTO {
+package com.Mindhub.homebanking.dtos;
+
+import com.Mindhub.homebanking.models.ClientLoan;
+
+public class ClientLoanDTO {
+    long id;
+    String nameLoan;
+    double amount;
+    int payments;
+
+    public ClientLoanDTO(ClientLoan clientLoan){
+        this.id = clientLoan.getId();
+        this.nameLoan = clientLoan.getNameLoan();
+        this.amount = clientLoan.getAmount();
+        this.payments = clientLoan.getPayments();
+    }
+
+    public long getId() {return id;}
+    public String getNameLoan() {return nameLoan;}
+    public double getAmount() {return amount;}
+    public int getPayments() {return payments;}
 }
