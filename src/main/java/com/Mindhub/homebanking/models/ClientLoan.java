db324d8311a6f78b2a1c922d6f2cb48ddb9288e3
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-20 10:19:48 -0300

    Remove property nameLoan

--- src/main/java/com/Mindhub/homebanking/models/ClientLoan.java
+++ src/main/java/com/Mindhub/homebanking/models/ClientLoan.java
@@ -10,7 +10,6 @@ public class ClientLoan {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
-    private String nameLoan;
     private int payments;
     private double amount;
     @ManyToOne(fetch = FetchType.EAGER)
@@ -21,15 +20,12 @@ public class ClientLoan {
     private Loan loan;
 
     public ClientLoan(){}
-    public ClientLoan(String nameLoan, double amount, int payments){
+    public ClientLoan(double amount, int payments){
         this.amount = amount;
-        this.nameLoan = nameLoan;
         this.payments = payments;
     }
     public int getPayments() {return payments;}
     public void setPayments(int payments) {this.payments = payments;}
-    public String getNameLoan() {return nameLoan;}
-    public void setNameLoan(String nameLoan) {this.nameLoan = nameLoan;}
     public long getId() {return id;}
     public double getAmount() {return amount;}
     public void setAmount(double amount) {this.amount = amount;}
