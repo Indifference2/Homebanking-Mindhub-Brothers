6ca24aa0e58fb100112ace82a453512d828bbbe5
Iván <102835891+Indifference2@users.noreply.github.com>
2023-04-25 14:06:24 -0300

    Created a RequestMapping to return all accounts client authenticated

--- src/main/java/com/Mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/Mindhub/homebanking/controllers/AccountController.java
@@ -2,8 +2,11 @@ package com.Mindhub.homebanking.controllers;
 
 import com.Mindhub.homebanking.dtos.AccountDTO;
 import com.Mindhub.homebanking.repositories.AccountRepository;
+import com.Mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,8 @@ import static java.util.stream.Collectors.toList;
 public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private ClientRepository clientRepository;
 
     @RequestMapping("/accounts")
     public List<AccountDTO> getAccounts(){
@@ -29,4 +34,15 @@ public class AccountController {
     public Optional<AccountDTO> getAccount(@PathVariable Long id){
         return Optional.of(new AccountDTO(accountRepository.findById(id).orElse(null)));
     }
+
+    @RequestMapping("/clients/current/accounts")
+    public List<AccountDTO> getAccountAuthenticated(Authentication authentication){
+        return clientRepository.findByEmail(
+                authentication.getName())
+                .getAccounts()
+                .stream()
+                .map(account -> new AccountDTO(account))
+                .collect(toList());
+    }
+
 }
