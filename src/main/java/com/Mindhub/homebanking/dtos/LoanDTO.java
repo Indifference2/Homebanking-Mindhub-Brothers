2e68744dcfdc97f7495818e8427beaa1ab24cf49
Iván <102835891+Indifference2@users.noreply.github.com>
2023-05-05 04:08:17 -0300

    create LoanDTO

---
+++ src/main/java/com/Mindhub/homebanking/dtos/LoanDTO.java
@@ -0,0 +1,27 @@
+package com.Mindhub.homebanking.dtos;
+
+import com.Mindhub.homebanking.models.Loan;
+
+import java.util.List;
+
+public class LoanDTO {
+    private long id;
+    private String name;
+    private double maxAmount;
+    private List<Integer> payments;
+
+    public LoanDTO(Loan loan){
+        this.id = loan.getId();
+        this.name = loan.getName();
+        this.maxAmount = loan.getMaxAmount();
+        this.payments = loan.getPayments();
+    }
+
+    public long getId() {return id;}
+
+    public String getName() {return name;}
+    public double getMaxAmount() {return maxAmount;}
+    public List<Integer> getPayments() {return payments;}
+
+
+}
